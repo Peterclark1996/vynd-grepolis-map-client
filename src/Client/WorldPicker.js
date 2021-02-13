2cfb265c836292ec512b87dc7558ccf2fd56dc70
pete <peterclark1996@gmail.com>
2021-02-13 11:46:32 +0000

    Add spinner when getting world list

--- src/Client/WorldPicker.js
+++ src/Client/WorldPicker.js
@@ -6,14 +6,17 @@ import Spinner from 'react-bootstrap/Spinner'
 const WorldToDisplay = (world) => world == null || world.name == null || world.code == null ? "No world selected" : world.name + " (" + world.code + ")"
 
 function WorldPicker({ world, worldList, setSelectedWorld, isWorldLoading }) {
+    const isWorldListLoading = worldList === null || worldList.length === 0
     return (
         <div>
-            <DropdownButton id="dropdown-basic-button" title={WorldToDisplay(world)}>
-                {worldList.map((w) => {
-                    return <Dropdown.Item key={w.code} onClick={() => setSelectedWorld(w)}>{WorldToDisplay(w)}</Dropdown.Item>
-                })}
-            </DropdownButton>
-            {isWorldLoading ? <Spinner animation="border" variant="primary" /> : <></>}
+            { isWorldListLoading || isWorldLoading ?
+                <Spinner animation="border" variant="primary" /> :
+                <DropdownButton id="dropdown-basic-button" title={WorldToDisplay(world)}>
+                    {worldList.map((w) => {
+                        return <Dropdown.Item key={w.code} onClick={() => setSelectedWorld(w)}>{WorldToDisplay(w)}</Dropdown.Item>
+                    })}
+                </DropdownButton>
+            }
         </div>
     )
 }
