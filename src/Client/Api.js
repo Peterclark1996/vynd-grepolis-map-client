8b7bc50eb5272e03ec9799f1dfeedb659cff2a09
pete <peterclark1996@gmail.com>
2021-03-11 19:33:25 +0000

    Fix url for map graphics

--- src/Client/Api.js
+++ src/Client/Api.js
@@ -1,4 +1,4 @@
-const GetUrl = () => !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'https://grepolis-live-map.herokuapp.com/'
+export const GetUrl = () => !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://localhost:5000/' : 'https://grepolis-live-map.herokuapp.com/'
 
 function Call(endpoint, params = {}) {
     return fetch(GetUrl() + endpoint + '?' + new URLSearchParams(params))
--- src/Client/View/OceanImages.js
+++ src/Client/View/OceanImages.js
@@ -1,4 +1,5 @@
 import { LayerGroup, ImageOverlay } from "react-leaflet"
+import { GetUrl } from "../Api"
 
 function OceanImages({ worldCode }) {
     return (
@@ -6,19 +7,19 @@ function OceanImages({ worldCode }) {
             {worldCode && worldCode.code ?
                 <LayerGroup>
                     <ImageOverlay
-                        url={"http://localhost:5000/getMapImage?world=" + worldCode.code + "&ocean=55"}
+                        url={GetUrl() + "getMapImage?world=" + worldCode.code + "&ocean=55"}
                         bounds={[[400, 500], [500, 600]]}
                     />
                     <ImageOverlay
-                        url={"http://localhost:5000/getMapImage?world=" + worldCode.code + "&ocean=44"}
+                        url={GetUrl() + "getMapImage?world=" + worldCode.code + "&ocean=44"}
                         bounds={[[500, 400], [600, 500]]}
                     />
                     <ImageOverlay
-                        url={"http://localhost:5000/getMapImage?world=" + worldCode.code + "&ocean=54"}
+                        url={GetUrl() + "getMapImage?world=" + worldCode.code + "&ocean=54"}
                         bounds={[[500, 500], [600, 600]]}
                     />
                     <ImageOverlay
-                        url={"http://localhost:5000/getMapImage?world=" + worldCode.code + "&ocean=45"}
+                        url={GetUrl() + "getMapImage?world=" + worldCode.code + "&ocean=45"}
                         bounds={[[400, 400], [500, 500]]}
                     />
                 </LayerGroup> :
