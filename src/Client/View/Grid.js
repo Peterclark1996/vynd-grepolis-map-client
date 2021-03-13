dfd71e23aa29ba46b2d527b4ab521c2a3d0ac47b
pete <peterclark1996@gmail.com>
2021-03-13 19:08:23 +0000

    Refactor and extend map graphics

--- src/Client/View/Grid.js
+++ src/Client/View/Grid.js
@@ -1,18 +1,21 @@
 import { LayerGroup, Polyline } from "react-leaflet"
 
 function Grid() {
+    const lineList = []
+    for (let i = 0; i < 11; i++) {
+        lineList.push(i * 100)
+    }
+
     return (
         <LayerGroup>
-            <Polyline positions={[[0, 300], [1000, 300]]} color="#000000" weight={1} />
-            <Polyline positions={[[0, 400], [1000, 400]]} color="#000000" weight={1} />
-            <Polyline positions={[[0, 500], [1000, 500]]} color="#000000" weight={1} />
-            <Polyline positions={[[0, 600], [1000, 600]]} color="#000000" weight={1} />
-            <Polyline positions={[[0, 700], [1000, 700]]} color="#000000" weight={1} />
-            <Polyline positions={[[300, 0], [300, 1000]]} color="#000000" weight={1} />
-            <Polyline positions={[[400, 0], [400, 1000]]} color="#000000" weight={1} />
-            <Polyline positions={[[500, 0], [500, 1000]]} color="#000000" weight={1} />
-            <Polyline positions={[[600, 0], [600, 1000]]} color="#000000" weight={1} />
-            <Polyline positions={[[700, 0], [700, 1000]]} color="#000000" weight={1} />
+            {lineList.map(l => {
+                return (
+                    <>
+                        <Polyline positions={[[0, 1000 - l], [1000, 1000 - l]]} color="#000000" weight={1} />
+                        <Polyline positions={[[l, 0], [l, 1000]]} color="#000000" weight={1} />
+                    </>
+                )
+            })}
         </LayerGroup>
     )
 }
--- src/Client/View/OceanImages.js
+++ src/Client/View/OceanImages.js
@@ -2,31 +2,38 @@ import { LayerGroup, ImageOverlay } from "react-leaflet"
 import { GetUrl } from "../Api"
 
 function OceanImages({ worldCode }) {
+    const oceanList = []
+    for (let x = 1; x < 9; x++) {
+        for (let y = 1; y < 9; y++) {
+            oceanList.push({
+                x: x, y: y
+            })
+        }
+    }
+
     return (
         <>
             {worldCode && worldCode.code ?
                 <LayerGroup>
-                    <ImageOverlay
-                        url={GetUrl() + "getMapImage?world=" + worldCode.code + "&ocean=55"}
-                        bounds={[[400, 500], [500, 600]]}
-                    />
-                    <ImageOverlay
-                        url={GetUrl() + "getMapImage?world=" + worldCode.code + "&ocean=44"}
-                        bounds={[[500, 400], [600, 500]]}
-                    />
-                    <ImageOverlay
-                        url={GetUrl() + "getMapImage?world=" + worldCode.code + "&ocean=54"}
-                        bounds={[[500, 500], [600, 600]]}
-                    />
-                    <ImageOverlay
-                        url={GetUrl() + "getMapImage?world=" + worldCode.code + "&ocean=45"}
-                        bounds={[[400, 400], [500, 500]]}
-                    />
+                    {oceanList.map(o => {
+                        return (<ImageOverlay
+                            url={GetUrl() + "getMapImage?world=" + worldCode.code + "&ocean=" + o.x + o.y}
+                            bounds={[
+                                [GetCoordsForOcean(o.x, o.y).coordX, GetCoordsForOcean(o.x, o.y).coordY],
+                                [GetCoordsForOcean(o.x, o.y).coordX + 100, GetCoordsForOcean(o.x, o.y).coordY + 100]
+                            ]}
+                        />)
+                    })}
                 </LayerGroup> :
                 <></>
             }
         </>
     )
 }
+
+const GetCoordsForOcean = (oceanX, oceanY) => ({
+    coordX: (9 - oceanY) * 100,
+    coordY: oceanX * 100
+})
 
 export default OceanImages
